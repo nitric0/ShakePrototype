30b2b7fe28b9c2d0199671e52fa5fe4b900f15b0
nitric0 <alisyedh80@gmail.com>
2020-02-22 19:39:07 -0600

    Prototype to test shake gesture to randomize list

--- ShakePrototype/ViewController.swift
+++ ShakePrototype/ViewController.swift
@@ -13,8 +13,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textView.backgroundColor = .darkGray
+        textView.textColor = .white
+        randomPersonText.textColor = .red
+        randomPersonText.textAlignment = .center
     }
-
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        self.randomPersons()
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    //List of people
+    var personList: [String] = []
+    
+    @IBAction func addPerson(_ sender: UIButton) {
+        textView.text = ""
+        var message = ""
+        if textView != nil{
+            //adding text field items into an array
+            if textField.hasText {
+                 personList.append(textField.text!)
+            }
+        }
+        
+        var i = 0
+        while i < personList.count {
+            message += "\(i + 1).  \(personList[i])\n"
+            i = i + 1
+        }
+        
+        textView.text = message
+        textField.text = ""
+    }
+    
+    @IBOutlet weak var randomPersonText: UITextView!
+    
+    func randomPersons() {
+        //var randomPerson = ""
+        for _ in 0...personList.count {
+            randomPersonText.text  = personList.randomElement()!
+        }
+        
+        //randomPersonText.text = randomPerson
+        
+    }
+    
 
 }
-
